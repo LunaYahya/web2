4ed64ae8489b73aab651035d0d969f631bbd7d44
Ayman-Yahia <lunayaha092@gmail.com>
2025-12-28 23:11:22 +0200

    add abilty to add task to the list array

--- script.js
+++ script.js
@@ -6,6 +6,48 @@ let tasks = [
   },
 ];
 
+let filter = "all";
+let currentEditIndex = null;
+let deleteIndex = null;
+
+function renderTasks() {
+  const list = document.getElementById("todoList");
+  list.innerHTML = "";
+
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "all") return true;
+    if (filter === "done") return task.done;
+    if (filter === "todo") return !task.done;
+  });
+
+  if (filter === "done" && filteredTasks.length === 0) {
+    list.innerHTML = `<div>No tasks.</div>`;
+    return;
+  }
+
+  filteredTasks.forEach((task, index) => {
+    const item = document.createElement("div");
+    item.className = "todo-item";
+    item.innerHTML = `
+              <div>
+                  <span class="${task.done ? "done" : ""}">${task.text}</span>
+              </div>
+              <div>
+                  <input type="checkbox" class="checkbox" ${
+                    task.done ? "checked" : ""
+                  } >
+                  <button >
+                      <i class="fa-regular fa-pen-to-square" style="color: #FFD43B;"></i>
+                  </button>
+                  <button onclick="initDeleteTask(${index})">
+                      <i class="fas fa-trash-alt"></i>  
+                  </button>
+              </div>
+          `;
+    list.appendChild(item);
+  });
+}
+
 function addTask() {
   const input = document.getElementById("todoInput");
   const taskText = input.value.trim();
@@ -19,4 +61,5 @@ function addTask() {
   tasks.push({ text: taskText, done: false });
   input.value = "";
   errorMessage.style.display = "none";
+  renderTasks();
 }
